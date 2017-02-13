02d3c500213b8b623112d19faf992025197df3d2
Xiaojue <designsor@gmail.com>
2017-02-13 15:19:19 +0800

    增加rollup和babel配置

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import babel from 'rollup-plugin-babel';
+var commonjs = require("rollup-plugin-commonjs");
+
+export default {
+    entry: 'src/index.js',
+    format: 'umd',
+    plugins: [
+        commonjs(),
+        babel({
+            babelrc: false,
+            exclude: 'node_modules/**',
+            presets: ['es2015-rollup', 'stage-0'],
+            plugins: ['transform-class-properties']
+        })
+    ],
+    dest: 'dist/ger.js'
+};
