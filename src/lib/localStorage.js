2ef90949ffc8619ef66da9bf9a60bfee96d38556
fuqiang6 <fuqiang6@staff.sina.com.cn>
2021-12-03 19:33:42 +0800

    增加 utils.parse「」「」

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -17,29 +17,33 @@ function callByArgs(func, args, global) {
 
 let storage = {
   //设置cookie内json的key名
-  getKey: function(errorObj) {
-    let isValid = function(name) {
+  getKey: function (errorObj) {
+    let isValid = function (name) {
       return errorObj[name];
     };
-    return ['msg', 'colNum', 'rowNum'].filter(isValid).map(isValid).join('@');
+    return ["msg", "colNum", "rowNum"].filter(isValid).map(isValid).join("@");
   },
   //检查是否有效
-  deleteExpiresItem: function(data) {
-    let oData = data ? utils.parse(data) : {};
+  deleteExpiresItem: function (data) {
+    try {
+      let oData = data ? utils.parse(data) : {};
 
-    let date = +new Date();
-    for (let key in oData) {
-      if (oData[key].expiresTime <= date) {
-        delete oData[key];
+      let date = +new Date();
+      for (let key in oData) {
+        if (oData[key].expiresTime <= date) {
+          delete oData[key];
+        }
       }
+    } catch (e) {
+      return {};
     }
     return oData;
   },
   //设置失效时间
-  getEpires: function(validTime) {
-    return +new Date() + (1000 * 60 * 60 * 24 * validTime);
+  getEpires: function (validTime) {
+    return +new Date() + 1000 * 60 * 60 * 24 * validTime;
   },
-  limitError: function(source, number) {
+  limitError: function (source, number) {
     let keys = Object.keys(source);
     if (keys.length >= number) {
       delete source[keys[0]];
@@ -47,7 +51,7 @@ let storage = {
     return source;
   },
   //获取cookie/localStorage内容体
-  setInfo: function(key, errorObj, validTime, max) {
+  setInfo: function (key, errorObj, validTime, max) {
     let source = storage.getItem(key);
     if (errorObj) {
       let name = storage.getKey(errorObj);
@@ -60,50 +64,67 @@ let storage = {
     return utils.stringify(source);
   },
   //设置cookie/localStorage
-  setItem: InertLocalFunc((...args) => {
-    try {
-      localStorage.setItem(args[0], callByArgs(storage.setInfo, args, storage));
-    } catch (e) {}
-  }, (...args) => {
-    utils.addCookie(args[0], callByArgs(storage.setInfo, args, storage));
-  }),
+  setItem: InertLocalFunc(
+    (...args) => {
+      try {
+        localStorage.setItem(
+          args[0],
+          callByArgs(storage.setInfo, args, storage)
+        );
+      } catch (e) {}
+    },
+    (...args) => {
+      utils.addCookie(args[0], callByArgs(storage.setInfo, args, storage));
+    }
+  ),
   //获取cookie/localStorage
-  getItem: InertLocalFunc((key) => {
-    return storage.deleteExpiresItem(localStorage.getItem(key));
-  }, (key) => {
-    return storage.deleteExpiresItem(utils.getCookie(key));
-  }),
+  getItem: InertLocalFunc(
+    (key) => {
+      return storage.deleteExpiresItem(localStorage.getItem(key));
+    },
+    (key) => {
+      return storage.deleteExpiresItem(utils.getCookie(key));
+    }
+  ),
   //清除cookie/localStorage
-  clear: InertLocalFunc((key) => {
-    return key ? localStorage.removeItem(key) : localStorage.clear();
-  }, (key) => {
-    return key ? utils.clearCookie(key) : document.cookie.split('; ').forEach(utils.clearCookie);
-  })
+  clear: InertLocalFunc(
+    (key) => {
+      return key ? localStorage.removeItem(key) : localStorage.clear();
+    },
+    (key) => {
+      return key
+        ? utils.clearCookie(key)
+        : document.cookie.split("; ").forEach(utils.clearCookie);
+    }
+  ),
 };
 
-
-
-
-let Localstroage = (supperclass) => class extends supperclass {
-  constructor(options) {
+let Localstroage = (supperclass) =>
+  class extends supperclass {
+    constructor(options) {
       super(options);
       this.setItem();
     }
     //得到元素值 获取元素值 若不存在则返回''
-  getItem(key) {
+    getItem(key) {
       return storage.getItem(key);
     }
     // 设置一条localstorage或cookie
-  setItem(errorObj) {
-    let _config = this.config;
-    storage.setItem(this.config.errorLSSign, errorObj, _config.validTime, _config.maxErrorCookieNo);
-    return utils.stringify(errorObj);
-  }
+    setItem(errorObj) {
+      let _config = this.config;
+      storage.setItem(
+        this.config.errorLSSign,
+        errorObj,
+        _config.validTime,
+        _config.maxErrorCookieNo
+      );
+      return utils.stringify(errorObj);
+    }
 
-  //清除ls/cookie 不传参数全部清空  传参之清当前ls/cookie
-  clear(key) {
-    storage.clear(key);
-  }
-};
+    //清除ls/cookie 不传参数全部清空  传参之清当前ls/cookie
+    clear(key) {
+      storage.clear(key);
+    }
+  };
 
 export default Localstroage;
